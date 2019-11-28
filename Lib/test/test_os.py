5539f0001fb2785c2d2feea97fc2a910ca954f92
Charalampos Stratakis <cstratak@redhat.com>
2019-11-28 17:26:02 +0100

    Fixups for FIPS compliant CSPRNG

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -28,6 +28,7 @@ import time
 import unittest
 import uuid
 import warnings
+from _hashlib import get_fips_mode
 from test import support
 from platform import win32_is_iot
 
@@ -1547,7 +1548,7 @@ class GetRandomTests(unittest.TestCase):
             else:
                 raise
         except ValueError as exc:
-            if exc.args[0] == "getrandom is not FIPS compliant":
+            if get_fips_mode() and exc.args[0] == "getrandom is not FIPS compliant":
                 raise unittest.SkipTest("Skip in FIPS mode")
             else:
                 raise
