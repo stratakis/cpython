cc2b987365e7970af167e4d07f11c8008aaab657
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2024-05-24 01:23:55 +0200

    00438: Fix ThreadedVSOCKSocketStreamTest (GH-119465) (GH-119479) (#119484)
    Fix ThreadedVSOCKSocketStreamTest: if get_cid() returns the host
    address or the "any" address, use the local communication address
    (loopback): VMADDR_CID_LOCAL.
    
    On Linux 6.9, apparently, the /dev/vsock device is now available but
    get_cid() returns VMADDR_CID_ANY (-1).
    
    (cherry picked from commit c750061047ee520d8299334df4b112fd983d7e48)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>
    (cherry picked from commit e94dbe4ed83460f18bd72563c5f09f6cdc71f604)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_socket.py
+++ Lib/test/test_socket.py
@@ -39,6 +39,7 @@ HOST = support.HOST
 MSG = 'Michael Gilfix was here\u1234\r\n'.encode('utf-8')
 MAIN_TIMEOUT = 60.0
 
+VMADDR_CID_LOCAL = 1
 VSOCKPORT = 1234
 AIX = platform.system() == "AIX"
 
@@ -112,8 +113,8 @@ def _have_socket_qipcrtr():
 
 def _have_socket_vsock():
     """Check whether AF_VSOCK sockets are supported on this host."""
-    ret = get_cid() is not None
-    return ret
+    cid = get_cid()
+    return (cid is not None)
 
 
 @contextlib.contextmanager
@@ -438,8 +439,6 @@ class ThreadedRDSSocketTest(SocketRDSTest, ThreadableTest):
 @unittest.skipIf(fcntl is None, "need fcntl")
 @unittest.skipUnless(HAVE_SOCKET_VSOCK,
           'VSOCK sockets required for this test.')
-@unittest.skipUnless(get_cid() != 2,
-          "This test can only be run on a virtual guest.")
 class ThreadedVSOCKSocketStreamTest(unittest.TestCase, ThreadableTest):
 
     def __init__(self, methodName='runTest'):
@@ -460,6 +459,9 @@ class ThreadedVSOCKSocketStreamTest(unittest.TestCase, ThreadableTest):
         self.cli = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
         self.addCleanup(self.cli.close)
         cid = get_cid()
+        if cid in (socket.VMADDR_CID_HOST, socket.VMADDR_CID_ANY):
+            # gh-119461: Use the local communication address (loopback)
+            cid = VMADDR_CID_LOCAL
         self.cli.connect((cid, VSOCKPORT))
 
     def testStream(self):
